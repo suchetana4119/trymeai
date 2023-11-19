6295335dbe592c8a2a1e5622261b3298bf42fae5
Suchetana Chatterjee <115644971+suchetana4119@users.noreply.github.com>
2023-11-19 14:49:45 +0000

    yes

--- Hello.py
+++ Hello.py
@@ -1,23 +1,48 @@
 import streamlit as st
 from PIL import Image
-from io import BytesIO
-import base64
+import rembg
+import io
 
-st.set_page_config(layout="wide", page_title="VITON")
+def remove_background(image_file):
+    input_img = image_file.read()
+    output_img = rembg.remove(input_img)
+    return Image.open(io.BytesIO(output_img))
 
-st.write("# Welcome to TryMeAI Virtual Try On!")
-st.sidebar.write("## Upload your image :gear:")
-st.markdown(
+def main():
+    st.title("Welcome to TryMeAI Virtual Try On!")
+    st.markdown(
         """
         Our VITON experience helps you try on clothing from the comfort of your home, with just a few clicks! Upload your reference image of yourself, upload a garment and see how it looks on you!
     """
     )
-MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
-col1, col2 = st.columns(2)
-my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+    st.sidebar.header("Upload Images")
+    uploaded_user_image = st.sidebar.file_uploader("Upload User Picture", type=["png", "jpg", "jpeg"])
+    uploaded_garment_image = st.sidebar.file_uploader("Upload Garment Picture", type=["png", "jpg", "jpeg"])
 
-st.sidebar.write("## Upload your garment :gear:")
+    if uploaded_user_image and uploaded_garment_image:
+        st.header("User Picture")
+        with st.spinner('Processing User Image...'):
+            processed_user_image = remove_background(uploaded_user_image)
+            st.image(processed_user_image, caption='User Picture (Background Removed)', use_column_width=True)
 
-col3, col4 = st.columns(2)
-my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"],key = "<uniquevalueofsomesort>")
+        st.header("Garment Picture")
+        with st.spinner('Processing Garment Image...'):
+            processed_garment_image = remove_background(uploaded_garment_image)
+            st.image(processed_garment_image, caption='Garment Picture (Background Removed)', use_column_width=True)
+
+        
+        resized_user_image = processed_user_image.resize((200, 200))  # Adjust size as needed
+        resized_garment_image = processed_garment_image.resize((200, 200))  # Adjust size as needed
+
+        
+        st.header("VITON experience!")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(resized_user_image, caption='User Image', use_column_width=False)
+        with col2:
+            st.image(resized_garment_image, caption='Garment Image', use_column_width=False)
+
+if __name__ == "__main__":
+    main()
+
